c44071e3ad1606cda109407b14d8ab930990f273
ndeshp2s <niranjan.deshpande@smail.inf.h-brs.de>
2015-08-27 15:36:47 +0200

    Changes made to executecb of move_robot. Stated machine added in executecb and removed from control callback.

--- inria_hbrs/semantic_navigation/move_robot/src/move_robot.cpp
+++ inria_hbrs/semantic_navigation/move_robot/src/move_robot.cpp
@@ -58,27 +58,26 @@ namespace move_robot
 
 	MoveRobot::~MoveRobot() 
 	{
-    if(as_ != NULL)
-      delete as_;
+      if(as_ != NULL)
+        delete as_;
 
-    if(planner_costmap_ != NULL)
-      delete planner_costmap_;
+      if(planner_costmap_ != NULL)
+        delete planner_costmap_;
 
-    if(controller_costmap_ != NULL)
-      delete controller_costmap_;
+      if(controller_costmap_ != NULL)
+        delete controller_costmap_;
 
-    planner_thread_->interrupt();
-    planner_thread_->join();
+      planner_thread_->interrupt();
+      planner_thread_->join();
 
-    delete planner_thread_;
+      delete planner_thread_;
 
-    delete planner_plan_;
-    delete latest_plan_;
-    delete controller_plan_;
+      delete planner_plan_;
+      delete latest_plan_;
+      delete controller_plan_;
 
-    planner_.reset();
-    controller_.reset();
-
+      planner_.reset();
+      controller_.reset();
 		
 	}
 
@@ -90,16 +89,36 @@ namespace move_robot
       as_->setAborted(move_base_msgs::MoveBaseResult(), "Aborting on goal because it was sent with an invalid quaternion");
       return;
     }
-    
+
     geometry_msgs::PoseStamped goal = goalToGlobalFrame(move_robot_goal->target_pose);
-    current_goal_pub_.publish(goal);
+    //current_goal_pub_.publish(goal);
 
     std::vector<geometry_msgs::PoseStamped> global_plan;
 
+    ros::Rate r(controller_frequency_);
     ros::NodeHandle n;
     while(n.ok())
     {
-      
+
+      if(as_->isNewGoalAvailable())
+      {
+          ROS_INFO("This action has received a new goal");
+          
+          move_base_msgs::MoveBaseGoal new_goal = *as_->acceptNewGoal();
+
+          if(!isQuaternionValid(new_goal.target_pose.pose.orientation))
+          {
+            as_->setAborted(move_base_msgs::MoveBaseResult(), "Aborting on goal because it was sent with an invalid quaternion");
+            return;
+          }
+            
+          goal = goalToGlobalFrame(new_goal.target_pose);
+          current_goal_pub_.publish(goal);
+
+          //Reset state for the next execution cycle
+          state_ = PLANNING;
+      }
+
       if(as_->isPreemptRequested())
       {
           if(as_->isNewGoalAvailable())
@@ -112,8 +131,9 @@ namespace move_robot
               as_->setAborted(move_base_msgs::MoveBaseResult(), "Aborting on goal because it was sent with an invalid quaternion");
               return;
             }
-            //ROS_INFO_STREAM("Got a goal");
+            
             goal = goalToGlobalFrame(new_goal.target_pose);
+            current_goal_pub_.publish(goal);
 
             //Reset state for the next execution cycle
             state_ = PLANNING;
@@ -141,23 +161,13 @@ namespace move_robot
       {
           //ROS_INFO_STREAM("Got a goal");
           goal = goalToGlobalFrame(goal);
+          current_goal_pub_.publish(goal);
 
           state_ = PLANNING;
 
-      /*  //we have a new goal so make sure the planner is awake
-        lock.lock();
-        planner_goal_ = goal;
-        runPlanner_ = true;
-        planner_cond_.notify_one();
-        lock.unlock();*/
-
       }
 
-      if(new_global_plan_)
-      {
-        state_ = CONTROLLING;
-      }
-
+      
       switch(state_)
       {
           case PLANNING:
@@ -180,10 +190,16 @@ namespace move_robot
                   return;
               }
 
+              r.sleep();
+
+              //make sure to sleep for the remainder of our cycle time
+              if(r.cycleTime() > ros::Duration(1 / controller_frequency_))
+                  ROS_WARN("Control loop missed its desired rate of %.4fHz... the loop actually took %.4f seconds", controller_frequency_, r.cycleTime().toSec());
+               
           }
           break;
 
-          case WAITING:
+          case STOP:
           {
               resetState();
 
@@ -197,22 +213,6 @@ namespace move_robot
           break;
 
       }
-
-    
-
-
-
-    /*  //the real work on pursuing a goal using local planner   
-      bool done = executeCycle(goal, global_plan);
-      
-
-      if(done)
-          return;
-
-      r.sleep();
-      //make sure to sleep for the remainder of our cycle time
-      if(r.cycleTime() > ros::Duration(1 / controller_frequency_))
-        ROS_WARN("Control loop missed its desired rate of %.4fHz... the loop actually took %.4f seconds", controller_frequency_, r.cycleTime().toSec());*/
 
     }
 
@@ -233,161 +233,6 @@ namespace move_robot
 
       publishZeroVelocity();
   }
-
-
-/*  void MoveRobot::executeCb(const move_base_msgs::MoveBaseGoalConstPtr& move_base_goal)
-  {
-    if(!isQuaternionValid(move_base_goal->target_pose.pose.orientation)){
-      as_->setAborted(move_base_msgs::MoveBaseResult(), "Aborting on goal because it was sent with an invalid quaternion");
-      return;
-    }
-
-    geometry_msgs::PoseStamped goal = goalToGlobalFrame(move_base_goal->target_pose);
-
-    //we have a goal so start the planner
-    boost::unique_lock<boost::mutex> lock(planner_mutex_);
-    planner_goal_ = goal;
-    runPlanner_ = true;
-    planner_cond_.notify_one();
-    lock.unlock();
-
-    //current_goal_pub_.publish(goal);
-    std::vector<geometry_msgs::PoseStamped> global_plan;
-
-    
-    
-
-    
-
-    ros::NodeHandle n;
-    while(n.ok())
-    {
-      
-
-      if(as_->isPreemptRequested()){
-        if(as_->isNewGoalAvailable()){
-          //if we're active and a new goal is available, we'll accept it, but we won't shut anything down
-          move_base_msgs::MoveBaseGoal new_goal = *as_->acceptNewGoal();
-
-          if(!isQuaternionValid(new_goal.target_pose.pose.orientation)){
-            as_->setAborted(move_base_msgs::MoveBaseResult(), "Aborting on goal because it was sent with an invalid quaternion");
-            return;
-          }
-
-          goal = goalToGlobalFrame(new_goal.target_pose);
-
-          //we'll make sure that we reset our state for the next execution cycle
-          
-          state_ = PLANNING;
-
-          //we have a new goal so make sure the planner is awake
-          lock.lock();
-          planner_goal_ = goal;
-          runPlanner_ = true;
-          planner_cond_.notify_one();
-          lock.unlock();
-
-          //publish the goal point to the visualizer
-          ROS_DEBUG_NAMED("move_base","move_base has received a goal of x: %.2f, y: %.2f", goal.pose.position.x, goal.pose.position.y);
-          //current_goal_pub_.publish(goal);
-
-          
-        }
-        
-      }
-
-      //we also want to check if we've changed global frames because we need to transform our goal pose
-      if(goal.header.frame_id != planner_costmap_->getGlobalFrameID()){
-        goal = goalToGlobalFrame(goal);
-
-        //we want to go back to the planning state for the next execution cycle
-        
-        state_ = PLANNING;
-
-        //we have a new goal so make sure the planner is awake
-        lock.lock();
-        planner_goal_ = goal;
-        runPlanner_ = true;
-        planner_cond_.notify_one();
-        lock.unlock();
-
-        //publish the goal point to the visualizer
-        ROS_DEBUG_NAMED("move_base","The global frame for move_base has changed, new frame: %s, new goal position x: %.2f, y: %.2f", goal.header.frame_id.c_str(), goal.pose.position.x, goal.pose.position.y);
-        //current_goal_pub_.publish(goal);
-
-        
-      }
-
-      if(new_global_plan_)
-      {
-        state_ = CONTROLLING;
-      }
-
-      if(goal_reached == true)
-      {
-        state_ = STOP;
-      }
-
-      //for timing that gives real time even in simulation
-      //ros::WallTime start = ros::WallTime::now();
-
-      //the real work on pursuing a goal is done here
-      //bool done = executeCycle(goal, global_plan);
-
-      //if we're done, then we'll return from execute
-      //if(done)
-      //  return;
-
-      switch(state_)
-      {
-        case PLANNING:
-        {
-          boost::mutex::scoped_lock lock(planner_mutex_);
-          runPlanner_ = true;
-          planner_cond_.notify_one();
-        }
-        //ROS_DEBUG_NAMED("move_base","Waiting for plan, in the planning state.");
-        break;
-
-        case STOP:
-
-        break;
-
-        //if we're controlling, we'll attempt to find valid velocity commands
-        case CONTROLLING:
-          bool done = executeCycle(goal, global_plan);
-          if(done)
-            return;
-
-        break;
-
-        
-        
-
-
-
-
-
-
-      }
-
-      
-    }
-
-    //wake up the planner thread so that it can exit cleanly
-    lock.lock();
-    runPlanner_ = true;
-    planner_cond_.notify_one();
-    lock.unlock();
-
-    //if the node is killed then we'll abort and return
-    as_->setAborted(move_base_msgs::MoveBaseResult(), "Aborting on the goal because the node has been killed");
-    return;
-  } */
-
-
-  
-  
 
 	
 };
